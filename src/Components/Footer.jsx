1ad350b28c5d42d9b623236d9fc6e031de0ab635
eayala199 <estanilaoa@gmail.com>
2023-06-26 23:05:33 -0300

    nuevo footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareFacebook, faInstagram, faWhatsapp, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer>
-      <p>Powered by</p>
-      <img src="./images/DH.png" alt='DH-logo' />
+      
+        <p>Powered by</p>
+        <img src="./images/DH.png" alt='DH-logo' className='foto' />
+      
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faSquareFacebook} size="2x" />
+        <FontAwesomeIcon icon={faInstagram} size="2x"/>
+        <FontAwesomeIcon icon={faWhatsapp} size="2x"/>
+        <FontAwesomeIcon icon={faTiktok} size="2x"/>
+      </div>
     </footer>
   )
 }
